b18b15bdd6304516473c1d4040d8eaa95f15cd0e
cakezero <sareahj.com@gmail.com>
2026-02-14 15:01:28 +0100

    auth route setup and moved the public profile route to route entrypoint

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { home } from "@/controllers/app.controller";
+import { userPublicProfile } from "@/controllers/user.controller";
+import { donorPublicProfile } from "@/controllers/donor.controller";
 import adminRoutes from "./admin.route";
 import appRoutes from "./app.route";
+import authRoutes from "./auth.route";
 import donorRoutes from "./donor.route";
 import userRoutes from "./user.route";
 import { authenticateDonor, authenticateAdmin, authenticateUser } from "@/middlewares/auth.middleware";
@@ -10,6 +13,9 @@ const router = Router();
 
 router
   .use("/", appRoutes)
+  .use("/auth", authRoutes)
+  .get("/donor/profile/:id", donorPublicProfile)
+  .get("/profile/:id", userPublicProfile)
   .use("/admin", authenticateAdmin, adminRoutes)
   .use("/donor", authenticateDonor, donorRoutes)
   .use("/recipient", authenticateUser, userRoutes);
